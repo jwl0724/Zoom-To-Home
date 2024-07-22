63046fb4a576fbcf58b1eeb4e66698f84c7b4745
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 02:23:34 -0700

    add logic to jump state and logic to switch between states

--- Scripts/States/PlayerStates/Jumping.cs
+++ Scripts/States/PlayerStates/Jumping.cs
@@ -1,16 +1,35 @@
+using Godot;
+
 namespace ZoomToHome { // MARKED FOR DELETION MAYBE, NEED TO SEE LATER
     public partial class Jumping : State {
-        // TODO IMPLEMENT LATER WHEN I FIGURE OUT WHAT TO DO WITH IT
+        private Player player;
+
+        public override void _Ready() {
+            player = parentBody as Player;
+        }
+
         public override void EnterState() {
-            throw new System.NotImplementedException();
+            player.ApplyForce(Vector3.Up * player.JumpSpeed, isOneShot: true);
         }
 
         public override void ExitState() {
-            throw new System.NotImplementedException();
+            
         }
 
-        protected override void ProcessAction() {
-            throw new System.NotImplementedException();
+        public override void ProcessInput(InputEvent inputEvent) {
+
+        }
+
+        public override void ProcessFrame(double delta) {
+
+        }
+
+        public override void ProcessPhysics(double delta) {
+            player.ApplyForce(Vector3.Down * player.Gravity, isOneShot: false);
+            player.SumForces();
+            player.MoveAndSlide();
+
+            if (player.Velocity.Y < 0) manager.ChangeState(manager.AllStates["Falling"]);
         }
     }
 }

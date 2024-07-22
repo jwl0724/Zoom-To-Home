fed3ae336f885a428237aef6ff6b92f989011620
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 02:23:20 -0700

    add logic to change between states

--- Scripts/States/PlayerStates/Idle.cs
+++ Scripts/States/PlayerStates/Idle.cs
@@ -1,26 +1,42 @@
+using Godot;
+
 namespace ZoomToHome {
     public partial class Idle : State {
-
         private Player player;
         public override void _Ready() {
             player = parentBody as Player;
         }
 
         public override void EnterState() {
-            // TO BE IMPLEMENTED WHEN VISUALS ARE ADDED
+            player.Velocity = new Vector3(0, player.Velocity.Y, 0);
         }
 
         public override void ExitState() {
             // TO BE IMPLEMENTED WHEN VISUALS ARE ADDED
         }
 
-        protected override void ProcessAction() {
-            if (manager.CurrentState is not Idle && player.Velocity.IsZeroApprox())
-                EmitSignal(SignalName.UpdateState, this);
+        public override void ProcessInput(InputEvent inputEvent) {
+            if (!player.IsOnFloor() || manager.CurrentState is Dead) return;
+            if (!Input.GetVector("left", "right", "forward", "backward").IsZeroApprox()) {
+                if (Input.IsActionPressed("sprint"))
+                    manager.ChangeState(manager.AllStates["Sprinting"]);
+                else manager.ChangeState(manager.AllStates["Running"]);
+            }
+            if (Input.IsActionJustPressed("jump"))
+                manager.ChangeState(manager.AllStates["Jumping"]);
         }
 
-        public override void _PhysicsProcess(double delta) {
-            ProcessAction();
+        public override void ProcessFrame(double delta) {
+        
+        }
+
+        public override void ProcessPhysics(double delta) {
+            player.ApplyForce(Vector3.Down * player.Gravity, isOneShot: false);
+            player.SumForces();
+            player.MoveAndSlide();
+            
+            if (!player.IsOnFloor() && manager.CurrentState is not Dead)
+                manager.ChangeState(manager.AllStates["Falling"]);
         }
     }
 }

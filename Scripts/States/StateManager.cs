35a3704b60353567e57200242f852807b194752d
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 02:22:00 -0700

    move processing to individual states

--- Scripts/States/StateManager.cs
+++ Scripts/States/StateManager.cs
@@ -1,20 +1,38 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace ZoomToHome {
     public partial class StateManager : Node {
+        [Signal] public delegate void StateChangeEventHandler();
         public State CurrentState { get; private set; }
+        public Dictionary<string, State> AllStates = new();
+        
         public override void _Ready() {
-            foreach (State child in GetChildren()) {
-                child.Connect(State.SignalName.UpdateState, 
-                    Callable.From((State newState) => ChangeState(newState))
-                );
+            foreach(Node child in GetChildren()) {
+                AllStates.Add(child.GetType().Name, child as State);
             }
+            ChangeState(AllStates["Idle"]);
         }
 
-        private void ChangeState(State state) {
+        public void ChangeState(State state) {
+            if (CurrentState?.GetType() == state.GetType()) return;
             CurrentState?.ExitState();
             CurrentState = state;
             state.EnterState();
+            EmitSignal(SignalName.StateChange, state);
+        }
+
+        public override void _PhysicsProcess(double delta) {
+            CurrentState.ProcessPhysics(delta);
+        }
+
+        public override void _Input(InputEvent inputEvent) {
+            CurrentState.ProcessInput(inputEvent);
+        }
+
+        public override void _Process(double delta) {
+            CurrentState.ProcessFrame(delta);
+            GD.Print(CurrentState.GetType().Name);
         }
     }
 }

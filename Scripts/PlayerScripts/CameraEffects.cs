b9cdf7d5275928180f3fc6a648d63a673c575758
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 02:19:15 -0700

    create script to handle camera effects depending on state

---
+++ Scripts/PlayerScripts/CameraEffects.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+namespace ZoomToHome {
+    public partial class CameraEffects : Camera3D {
+        public static float DefaultFOV;
+        private static readonly int FOVChangeSpeed = 50;
+        private static readonly int minFOVDifference = 5;
+        private StateManager playerStateManager;
+        public override void _Ready() {
+            Owner.Connect(SignalName.Ready, Callable.From(() => 
+                playerStateManager = (Owner as Player).StateManager
+            ));
+            DefaultFOV = Fov;
+        }
+        public override void _Process(double delta) {
+            HandleCameraFOV((float) delta);
+        }
+
+        private void HandleCameraFOV(float delta) {
+            if (playerStateManager.CurrentState is Sprinting) {
+                if (Fov < DefaultFOV - minFOVDifference) return;
+                Fov -= delta * FOVChangeSpeed;
+
+            } else if (Fov < DefaultFOV) Fov += delta * FOVChangeSpeed;
+        }
+    }
+}

723f072c0c65d1003816a01ea007a031e0a09e12
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 02:21:04 -0700

    add functions for input, frame, and physics processing

--- Scripts/States/State.cs
+++ Scripts/States/State.cs
@@ -4,9 +4,10 @@ namespace ZoomToHome {
     public abstract partial class State : Node {
         [Export] protected PhysicsBody3D parentBody;
         [Export] protected StateManager manager;
-        [Signal] public delegate void UpdateStateEventHandler();
         public abstract void EnterState();
         public abstract void ExitState();
-        protected abstract void ProcessAction();
+        public abstract void ProcessInput(InputEvent inputEvent);
+        public abstract void ProcessFrame(double delta);
+        public abstract void ProcessPhysics(double delta);
     }
 }

7fb47c669d6dec2033c70d9da71cf793474d102f
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 21:01:38 -0700

    implement movement aspect of zip

--- Scripts/States/PlayerStates/Zipping.cs
+++ Scripts/States/PlayerStates/Zipping.cs
@@ -3,12 +3,21 @@ using System;
 
 namespace ZoomToHome {
     public partial class Zipping : State {
+        private Vector3 grapplePoint;
+        private Player player;
+
+        public override void _Ready() {
+            player = parentBody as Player;
+        }
+
         public override void EnterState() {
-            
+            grapplePoint = player.GetRaycastImpactPoint();
+            if (!grapplePoint.IsFinite()) 
+                manager.ChangeState(manager.PreviousState);
         }
 
         public override void ExitState() {
-            
+            grapplePoint = Vector3.Inf; // set point to something invalid
         }
 
         public override void ProcessFrame(double delta) {
@@ -16,11 +25,15 @@ namespace ZoomToHome {
         }
 
         public override void ProcessInput(InputEvent inputEvent) {
-            
+            if (!Input.IsActionJustReleased("zip")) return;
+            if (player.IsOnFloor()) manager.ChangeState(manager.AllStates["Recovering"]);
+            else if (player.Velocity.Y > 0) manager.ChangeState(manager.AllStates["Jumping"]);
+            else if (player.Velocity.Y < 0) manager.ChangeState(manager.AllStates["Falling"]);
         }
 
         public override void ProcessPhysics(double delta) {
-            
+            player.Velocity = player.Position.DirectionTo(grapplePoint) * player.ZipRetractSpeed;
+            player.MoveAndSlide();
         }
     }
 }

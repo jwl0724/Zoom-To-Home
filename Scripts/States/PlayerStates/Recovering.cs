81a308112ddb6033ce1fbbfe56829a1fde926f89
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 21:04:38 -0700

    add sliding logic to recovering state

--- Scripts/States/PlayerStates/Recovering.cs
+++ Scripts/States/PlayerStates/Recovering.cs
@@ -2,7 +2,16 @@ using Godot;
 using System;
 
 namespace ZoomToHome {
+    // TODO: CONNECT FALLING AND JUMPING STATES TO RECOVERY
+    // TODO: IMPLEMENT CROUCH (FOR SLIDING MECHS)
+    // TODO: IMPLEMENT SWINGING
     public partial class Recovering : State {
+        private Player player;
+
+        public override void _Ready() {
+            player = parentBody as Player;
+        }
+
         public override void EnterState() {
             
         }
@@ -16,11 +25,26 @@ namespace ZoomToHome {
         }
 
         public override void ProcessInput(InputEvent inputEvent) {
-            
+            if (Input.IsActionJustPressed("jump")) manager.ChangeState(manager.AllStates["Jumping"]);
+            if (Input.IsActionJustPressed("zip")) manager.ChangeState(manager.AllStates["Zipping"]);
+            // if (Input.IsActionJustPressed("swing")) manager.ChangeState(manager.AllStates["Swinging"]);
         }
 
         public override void ProcessPhysics(double delta) {
+            player.Velocity *= player.FrictionCoefficient;
+            player.MoveAndSlide();
             
+            if (!player.IsOnFloor()) {
+                manager.ChangeState(manager.AllStates["Falling"]);
+                return;
+            }
+
+            if (player.Velocity.Length() > player.MoveSpeed) return;
+            if (Input.GetVector("left", "right", "forward", "backward").IsZeroApprox()) {
+                if (Input.IsActionPressed("sprint")) manager.ChangeState(manager.AllStates["Sprinting"]);
+                else manager.ChangeState(manager.AllStates["Running"]);
+            }
+            if (player.Velocity.IsZeroApprox()) manager.ChangeState(manager.AllStates["Idle"]);
         }
     }
 }

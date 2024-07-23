ec7251ac3413f0995b272b6505291cf6ff818a7c
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-23 11:58:15 -0700

    implement crouching logic

--- Scripts/States/PlayerStates/Crouch.cs
+++ Scripts/States/PlayerStates/Crouch.cs
@@ -3,12 +3,18 @@ using System;
 
 namespace ZoomToHome {
     public partial class Crouch : State {
+        private Player player;
+        public override void _Ready() {
+            player = parentBody as Player;
+        }
+
         public override void EnterState() {
-            
+            if (player.Velocity.Length() > player.MoveSpeed) player.Velocity *= 1.3f;
+            player.ToggleCrouch(true);
         }
 
         public override void ExitState() {
-            
+            player.ToggleCrouch(false);
         }
 
         public override void ProcessFrame(double delta) {
@@ -16,11 +22,29 @@ namespace ZoomToHome {
         }
 
         public override void ProcessInput(InputEvent inputEvent) {
-            
+            if (!Input.IsActionJustReleased("crouch")) return;
+            else if (Input.IsActionJustPressed("jump")) manager.ChangeState(manager.AllStates["Jumping"]);
+            else if (Input.IsActionJustPressed("zip")) manager.ChangeState(manager.AllStates["Zipping"]);
+            else if (Input.IsActionJustPressed("swing")) manager.ChangeState(manager.AllStates["Swinging"]);
+            else if (Input.IsActionPressed("sprint")) manager.ChangeState(manager.AllStates["Sprinting"]);
+
+            if (player.Velocity.IsZeroApprox()) manager.ChangeState(manager.AllStates["Idle"]);
+            else if (Input.GetVector("left", "right", "forward", "backward").IsZeroApprox())
+                manager.ChangeState(manager.AllStates["Recovering"]);
+            else manager.ChangeState(manager.AllStates["Running"]);
         }
 
         public override void ProcessPhysics(double delta) {
-            
+            player.ApplyForce(Vector3.Down * player.Gravity, isOneShot: false);
+            if (player.Velocity.Length() > player.MoveSpeed / 2) {
+                player.Velocity *= player.SlideFrictionCoefficient;
+                if (Input.IsActionPressed("left")) player.Velocity += Vector3.Left * player.MoveSpeed * (float) delta;
+                else if (Input.IsActionPressed("right")) player.Velocity += Vector3.Right * player.MoveSpeed * (float) delta;
+
+            } else player.SetVelocityToInputVector(player.MoveSpeed / 2);
+            player.SumForces();
+            player.MoveAndSlide();
+            if (!player.IsOnFloor()) manager.ChangeState(manager.AllStates["Falling"]);
         }
     }
 }

1d7ccdd4fded299fbaebaa7bb89ca217d8ba2324
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 19:45:45 -0700

    create helper functions that states call

--- Scripts/PlayerScripts/Player.cs
+++ Scripts/PlayerScripts/Player.cs
@@ -4,7 +4,7 @@ using Godot;
 namespace ZoomToHome {
     public partial class Player : Entity {
         public StateManager StateManager;
-        public PlayerRotationManager RotationHelper;
+        private PlayerRotationManager RotationHelper;
         private readonly LinkedList<Vector3> forceList = new();
 
         public override void _Ready() {
@@ -25,6 +25,30 @@ namespace ZoomToHome {
         public void ApplyForce(Vector3 force, bool isOneShot) {
             if (isOneShot) forceList.AddFirst(force);
             else forceList.AddFirst(force * (float) GetProcessDeltaTime());
+        }
+
+        public void SetVelocityToInputVector(float moveSpeed) {
+            Vector2 inputVector = Input.GetVector("left", "right", "forward", "backward");
+            Vector3 movementDirection = GetForwardVectorOnHorizontalPlane(new Vector3(inputVector.X, 0, inputVector.Y), moveSpeed);
+            Velocity = new(movementDirection.X, Velocity.Y, movementDirection.Z);
+        }
+
+        public void ApplyMidAirInputs(float adjustSpeed) {
+            Vector2 inputVector = Input.GetVector("left", "right", "forward", "backward");
+            if (inputVector.IsZeroApprox()) return;
+
+            Vector3 horizontalVelocity = new(Velocity.X, 0, Velocity.Z);
+            Vector3 movementDirection = GetForwardVectorOnHorizontalPlane(new Vector3(inputVector.X, 0, inputVector.Y), adjustSpeed);
+            Vector3 summedVector = horizontalVelocity + movementDirection * (float) GetProcessDeltaTime();
+
+            if (Mathf.Abs(summedVector.Length()) < MoveSpeed || movementDirection.Dot(horizontalVelocity) <= 0)
+                ApplyForce(movementDirection, isOneShot: false);
+        }
+
+        public Vector3 GetForwardVectorOnHorizontalPlane(Vector3 vector, float moveSpeed) {
+            Vector3 forwardVector = RotationHelper.Transform.Basis * new Vector3(vector.X, 0, vector.Z);
+            Vector3 normalizedVector = new(forwardVector.X, 0, forwardVector.Z);
+            return normalizedVector.Normalized() * moveSpeed;
         }
     }
 }

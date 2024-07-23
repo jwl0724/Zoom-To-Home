719e52b11eef85139ec618a0155f983f03273deb
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-23 12:00:21 -0700

    implement slight sliding when moving very fast

--- Scripts/States/PlayerStates/Recovering.cs
+++ Scripts/States/PlayerStates/Recovering.cs
@@ -27,22 +27,29 @@ namespace ZoomToHome {
         public override void ProcessInput(InputEvent inputEvent) {
             if (Input.IsActionJustPressed("jump")) manager.ChangeState(manager.AllStates["Jumping"]);
             if (Input.IsActionJustPressed("zip")) manager.ChangeState(manager.AllStates["Zipping"]);
-            // if (Input.IsActionJustPressed("swing")) manager.ChangeState(manager.AllStates["Swinging"]);
+            if (Input.IsActionJustPressed("swing")) manager.ChangeState(manager.AllStates["Swinging"]);
+            if (Input.IsActionJustPressed("crouch")) manager.ChangeState(manager.AllStates["Crouch"]);
         }
 
         public override void ProcessPhysics(double delta) {
-            player.Velocity *= player.FrictionCoefficient;
+            Vector2 inputVector = Input.GetVector("left", "right", "forward", "backward");
+            Vector3 movementDirection = player.GetForwardVectorOnHorizontalPlane(
+                new Vector3(inputVector.X, 0, inputVector.Y), player.MoveSpeed
+            );
+
+            player.Velocity = player.Velocity.Lerp(Vector3.Zero, 0.025f);
+            player.ApplyForce(movementDirection, isOneShot: false);
+            player.SumForces();
             player.MoveAndSlide();
-            
+                      
             if (!player.IsOnFloor()) {
                 manager.ChangeState(manager.AllStates["Falling"]);
                 return;
             }
-
-            if (player.Velocity.Length() > player.MoveSpeed) return;
-            if (Input.GetVector("left", "right", "forward", "backward").IsZeroApprox()) {
+            if (player.Velocity.Length() > player.MoveSpeed * player.SprintMultiplier) return;
+            if (!Input.GetVector("left", "right", "forward", "backward").IsZeroApprox()) {
                 if (Input.IsActionPressed("sprint")) manager.ChangeState(manager.AllStates["Sprinting"]);
-                else manager.ChangeState(manager.AllStates["Running"]);
+                else if (player.Velocity.Length() < player.MoveSpeed) manager.ChangeState(manager.AllStates["Running"]);
             }
             if (player.Velocity.IsZeroApprox()) manager.ChangeState(manager.AllStates["Idle"]);
         }

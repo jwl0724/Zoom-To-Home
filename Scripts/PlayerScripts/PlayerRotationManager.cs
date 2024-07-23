a58f248f05ca1be9ad518df97e64f436d1a2209a
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-22 21:00:15 -0700

    create helper function to get impact point of raycast

--- Scripts/PlayerScripts/PlayerRotationManager.cs
+++ Scripts/PlayerScripts/PlayerRotationManager.cs
@@ -3,14 +3,22 @@ using Godot;
 namespace ZoomToHome {
     public partial class PlayerRotationManager : Node3D {
         private Player player;
+        private RayCast3D raycast;
         public override void _Ready() {
             player = Owner as Player;
+            raycast = GetNode<RayCast3D>("Raycast");
         }
 
         public override void _Input(InputEvent mouseMovement) {
             if (mouseMovement is not InputEventMouseMotion movement || Input.MouseMode != Input.MouseModeEnum.Captured) 
                 return;
             else ProcessMouseMovement(movement);
+        }
+
+        public Vector3 GetRaycastImpactPoint() {
+            var collidingBody = raycast.GetCollider();
+            if (collidingBody is not StaticBody3D) return Vector3.Inf;
+            return raycast.GetCollisionPoint();
         }
 
         private void ProcessMouseMovement(InputEventMouseMotion movement) {

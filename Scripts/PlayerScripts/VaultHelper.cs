c93add54ac07031719e210bff25a316e2dc22212
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-28 03:01:40 -0700

    add logic to vault helper for vaulting functionality

--- Scripts/PlayerScripts/VaultHelper.cs
+++ Scripts/PlayerScripts/VaultHelper.cs
@@ -3,18 +3,30 @@ using System;
 
 namespace ZoomToHome {
     public partial class VaultHelper : Node3D {
-        private RayCast3D topRaycast;
-        private RayCast3D botRaycast;
+        [Export] private PlayerRotationManager rotationalHelper;
+        private RayCast3D vaultRaycast;
 
         public override void _Ready() {
-            topRaycast = GetNode<RayCast3D>("Top");
-            botRaycast = GetNode<RayCast3D>("Bottom");
+            vaultRaycast = GetNode<RayCast3D>("Vault Raycast");
         }
 
-        public bool ValidVaulting() {
-            return !topRaycast.IsColliding() && botRaycast.IsColliding();
+        public override void _Process(double delta) {
+            GlobalRotation = Vector3.Up * rotationalHelper.Rotation.Y;
+            GlobalPosition = (Owner as Player).GlobalPosition;
         }
 
+        public bool CanVault() {
+            return vaultRaycast.IsColliding();
+        }
 
+        // POSSIBLE BUG WHERE YOU CLIP INTO GROUND, MARKED FOR OBSERVATION
+        public Vector3 CalculateVaultLocation() {
+            Vector3 forwardVector = rotationalHelper.Transform.Basis * Vector3.Forward;
+            Vector3 flattenedVectorForward = new (forwardVector.X, 0, forwardVector.Z);
+            flattenedVectorForward = flattenedVectorForward.Normalized() * 1.5f;
+
+            Vector3 vaultDestination = vaultRaycast.GetCollisionPoint() + flattenedVectorForward;
+            return vaultDestination;
+        }
     }
 }

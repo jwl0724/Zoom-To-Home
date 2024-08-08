079d069a51708243b7d6242dfe94ca23039bb792
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-08 12:04:56 -0700

    add more special animations

--- Scripts/PlayerScripts/PlayerAnimator.cs
+++ Scripts/PlayerScripts/PlayerAnimator.cs
@@ -2,11 +2,12 @@ using Godot;
 using System;
 using System.Linq;
 
+// TODO: IN BLENDER, EITHER DELETE THE HEAD OF THE MODEL, OR SEPARATE THE HEAD INTO DIFFERENT COMPONENT
 namespace ZoomToHome {
     public partial class PlayerAnimator : Node3D {
         [Export] private Player player;
         [Export] private AnimationPlayer animation;
-        private string[] specialAnimations = {"Stand", "Swing", "Zip"};
+        private string[] specialAnimations = {"Stand", "Swing", "Zip", "Stop", "CrouchIdle"};
 
         public override void _Process(double delta) {
             if (player.StateManager.CurrentState is WallRunning) return; // BUG: NEED TO FIX ROTATION IF PLAYER NOT LOOKING PROPERLY WHILE ATTACHING TO WALL
@@ -16,6 +17,7 @@ namespace ZoomToHome {
         public void Play(string animationName, bool playOver = true) {
             if (specialAnimations.Contains(animationName)) {
                 // handle special animations
+                if (!playOver && animation.IsPlaying()) return;
                 switch(animationName) {
                     case "Stand":
                         animation.PlayBackwards("Crouch");
@@ -24,8 +26,15 @@ namespace ZoomToHome {
                         break;
                     case "Zip":
                         break;
+                    case "Stop":
+                        animation.Stop(keepState: true);
+                        break;
+                    case "CrouchIdle":
+                        if (animation.AssignedAnimation == "Crouch") return;
+                        animation.Pause();
+                        break;
                     default:
-                        GD.PushError("Something went horribly wrong, this should never run");
+                        GD.PushError($"You forgot to add a switch case for {animationName} stupid");
                         break;
                 }
 

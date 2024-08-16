fcbe7efc39b62cf8a49e62646a1c4228097fb7d0
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-16 16:27:53 -0700

    add fromEnd parameter, and add stop public method

--- Scripts/PlayerScripts/HandAnimator.cs
+++ Scripts/PlayerScripts/HandAnimator.cs
@@ -4,9 +4,13 @@ namespace ZoomToHome {
     public partial class HandAnimator : Node3D {
         [Export] private AnimationPlayer animation;
 
-        public void Play(string animationName, float playSpeedScale = 1, bool playOver = true) {
+        public void Play(string animationName, float playSpeedScale = 1, bool playOver = true, bool fromEnd = false) {
             if(!playOver && animation.IsPlaying()) return;
-            animation.Play(animationName, customSpeed: playSpeedScale);
+            animation.Play(animationName, customSpeed: playSpeedScale, fromEnd: fromEnd);
+        }
+
+        public void Stop() {
+            animation.Stop();
         }
     }
 }

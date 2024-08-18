19692c74989df829529d9c3292eed1a76b4b23ee
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-17 17:23:34 -0700

    begin work on implementing look enforcement

--- Scripts/PlayerScripts/PlayerRotationManager.cs
+++ Scripts/PlayerScripts/PlayerRotationManager.cs
@@ -2,6 +2,11 @@ using Godot;
 
 namespace ZoomToHome {
     public partial class PlayerRotationManager : Node3D {
+        public bool EnableInput { get; set; } = true;
+        public bool EnableRotationEnforcement { get; set; } = false;
+        public Vector3 LookPoint { get; set; } = Vector3.Inf;
+        public bool IsGlobalPoint { get; set; } = true; 
+        public float EnforceWeight { get; set; } = 1;
         private Player player;
         private RayCast3D raycast;
         public override void _Ready() {
@@ -22,6 +27,8 @@ namespace ZoomToHome {
         }
 
         private void ProcessMouseMovement(InputEventMouseMotion movement) {
+            if (!EnableInput) return;
+
             // looking left/right
             float rotationDegreesY = Rotation.Y + Mathf.DegToRad(-movement.Relative.X * player.MouseSensitivity);
 
@@ -32,6 +39,21 @@ namespace ZoomToHome {
             );
 
             Rotation = new Vector3(rotationDegreesX, rotationDegreesY, 0);
-        } 
+            // TODO: FIX THIS GARBAGE
+            // if (EnableRotationEnforcement) {
+            //     if (IsGlobalPoint) {
+            //         float angleX = new Vector2(GlobalPosition.X, GlobalPosition.Y).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Y));
+            //         float angleY = new Vector2(GlobalPosition.X, GlobalPosition.Z).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Z));
+            //         Rotation = Rotation.Lerp(new Vector3(angleX, angleY, 0), EnforceWeight);
+            //         // Rotation = Rotation.Lerp(GlobalPosition.DirectionTo(LookPoint), EnforceWeight);
+
+            //     } else {
+            //         float angleX = new Vector2(Position.X, Position.Y).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Y));
+            //         float angleY = new Vector2(Position.X, Position.Z).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Z));
+            //         Rotation = Rotation.Lerp(new Vector3(angleX, angleY, 0), EnforceWeight);
+            //         // Rotation = Rotation.Lerp(Position.DirectionTo(LookPoint), EnforceWeight);
+            //     }
+            // }
+        }
     }
 }

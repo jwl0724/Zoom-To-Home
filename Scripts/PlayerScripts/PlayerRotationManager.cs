0124401ea064ae323756d99696b106d60becc108
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-18 18:41:56 -0700

    finish non-global look enforcement

--- Scripts/PlayerScripts/PlayerRotationManager.cs
+++ Scripts/PlayerScripts/PlayerRotationManager.cs
@@ -20,6 +20,36 @@ namespace ZoomToHome {
             else ProcessMouseMovement(movement);
         }
 
+        public override void _PhysicsProcess(double delta) {
+            // TODO: FIX THIS GARBAGE
+            if (EnableRotationEnforcement) {
+                if (IsGlobalPoint) {
+                    
+                    Vector3 oldRotation = Rotation;
+                    if (oldRotation.Y < 0) oldRotation += Vector3.Up * 2 * Mathf.Pi;
+                    
+                    LookAt(LookPoint);
+                    
+                    Vector3 newRotation = new(Rotation.X, Rotation.Y, 0);
+                    if (newRotation.Y < 0) {
+                        newRotation += Vector3.Up * 2 * Mathf.Pi;
+                    }
+                    Rotation = oldRotation.Lerp(newRotation, EnforceWeight);
+                    GD.Print($"old rot = {oldRotation.Y}, new rot = {newRotation.Y}, lerp rot = {Rotation.Y}");
+
+                } else {
+                    Vector3 oldRotation = Rotation;
+                    if (oldRotation.Y < 0) oldRotation += Vector3.Up * 2 * Mathf.Pi;
+
+                    LookAt(LookPoint + GlobalPosition);
+
+                    Vector3 newRotation = new(Rotation.X, Rotation.Y, 0);
+                    if (newRotation.Y < 0) newRotation += Vector3.Up * 2 * Mathf.Pi;
+                    Rotation = oldRotation.Lerp(newRotation, EnforceWeight);
+                }
+            }
+        }
+
         public Vector3 GetRaycastImpactPoint() {
             var collidingBody = raycast.GetCollider();
             if (collidingBody is not StaticBody3D) return Vector3.Inf;
@@ -37,23 +67,7 @@ namespace ZoomToHome {
                 Mathf.DegToRad(-movement.Relative.Y * player.MouseSensitivity) + Rotation.X,
                 Mathf.DegToRad(-89.99f), Mathf.DegToRad(89.99f)
             );
-
             Rotation = new Vector3(rotationDegreesX, rotationDegreesY, 0);
-            // TODO: FIX THIS GARBAGE
-            // if (EnableRotationEnforcement) {
-            //     if (IsGlobalPoint) {
-            //         float angleX = new Vector2(GlobalPosition.X, GlobalPosition.Y).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Y));
-            //         float angleY = new Vector2(GlobalPosition.X, GlobalPosition.Z).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Z));
-            //         Rotation = Rotation.Lerp(new Vector3(angleX, angleY, 0), EnforceWeight);
-            //         // Rotation = Rotation.Lerp(GlobalPosition.DirectionTo(LookPoint), EnforceWeight);
-
-            //     } else {
-            //         float angleX = new Vector2(Position.X, Position.Y).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Y));
-            //         float angleY = new Vector2(Position.X, Position.Z).AngleToPoint(new Vector2(LookPoint.X, LookPoint.Z));
-            //         Rotation = Rotation.Lerp(new Vector3(angleX, angleY, 0), EnforceWeight);
-            //         // Rotation = Rotation.Lerp(Position.DirectionTo(LookPoint), EnforceWeight);
-            //     }
-            // }
         }
     }
 }

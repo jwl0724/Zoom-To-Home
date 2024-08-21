4b38410d536a1a0bed836f92af4fdaabc8af8592
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-20 18:39:25 -0700

    jokes

--- Scripts/Entity.cs
+++ Scripts/Entity.cs
@@ -2,9 +2,15 @@ using Godot;
 
 namespace ZoomToHome {
     public partial class Entity : CharacterBody3D {
+        [Signal] public delegate void DamagedEventHandler();
+
         public float MoveSpeed { get; set; }
         public float Gravity { get; set; }
         public float SprintMultiplier { get; set; }
         public float SFXVolumeMultiplier { get; set; }
+
+        public void HitEntity() {
+            EmitSignal(SignalName.Damaged);
+        }
     }
 }

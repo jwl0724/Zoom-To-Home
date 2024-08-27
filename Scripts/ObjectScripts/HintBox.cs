76df89340040bd2892ea9c7ed47b78953243a2e8
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-08-26 18:41:47 -0700

    add beginning implementations of hint box

--- Scripts/ObjectScripts/HintBox.cs
+++ Scripts/ObjectScripts/HintBox.cs
@@ -2,12 +2,14 @@ using Godot;
 
 namespace ZoomToHome {
     public partial class HintBox : Node3D {
-        [Export] string hintText;
+        [Export] private Label hintText;
+        private DialogueBox dialogueBox;
         private Area3D hintArea;
 
         public override void _Ready() {
             hintArea = GetNode("Hint Area") as Area3D;
             hintArea.Connect(Area3D.SignalName.BodyEntered, Callable.From((Node3D body) => DisplayHint()));
+            dialogueBox = GetNode("Hint Dialogue") as DialogueBox;
         }
 
         public override void _PhysicsProcess(double delta) {
@@ -15,8 +17,13 @@ namespace ZoomToHome {
         }
 
         private void DisplayHint() {
-            // WILL ADD LATER
-            GD.Print(hintText);
+            string dialogueText = TranslateText(hintText.Text);
+            dialogueBox.ShowDialogue(dialogueText);
+        }
+
+        private string TranslateText(string text) {
+            // IMPLEMENT LATER, WHEN DIFFERENT KEYBINDS FOR ACTIONS ARE IMPLEMENTED
+            return text;
         }
     }
 }

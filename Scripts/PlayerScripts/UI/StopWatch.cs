1e823a7be76d078bbec9b2a9c8c84bde17e310b4
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-09-01 22:05:07 -0700

    create UI element for timing

---
+++ Scripts/PlayerScripts/UI/StopWatch.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+namespace ZoomToHome {
+    public partial class StopWatch : Label {
+        private float timeElapsed = 0f;
+        private bool running = false;
+
+        public override void _Ready() {
+            GetTree().CurrentScene.Connect(Node.SignalName.Ready, Callable.From(() => Resume()));
+        }   
+
+        public override void _Process(double delta) {
+            if (running == false) return;
+            Text = ConvertFormat(timeElapsed);
+            timeElapsed += (float) delta;
+        }
+
+        public void ResetWatch() {
+            timeElapsed = 0f;
+        }
+
+        public float Stop() {
+            running = false;
+            return timeElapsed;
+        }
+
+        public void Resume() {
+            running = true;
+        }
+
+        private static string ConvertFormat(float time) {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+            return string.Format(@"{0:mm\:ss\.ff}", timeSpan);
+        }
+    }
+}
